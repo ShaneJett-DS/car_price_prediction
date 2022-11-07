a89c99dc7acda038d880c4c0c4ffa6e7442a5001
Shane <shanejett.ds@gmail.com>
2022-11-07 18:42:55 -0500

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
 
 
-st.write("hello")
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+random = st.beta_container()
+
+
+with header:
+    st.title("Car Price Prediction")
+
+with dataset:
+    st.header("Test header")
+    st.text("Some descriptive text")
+
+
+
+
+
+st.write("hello, hello")
