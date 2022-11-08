78910d0bce5cf4436a452949eff9115e3adc589b
Shane <shanejett.ds@gmail.com>
2022-11-07 19:17:31 -0500

    update
    Uploaded dataset and modified streamlit file

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import os
 
+cwd = os.cwd()
 
 header = st.container()
 dataset = st.container()
@@ -14,6 +16,8 @@ with header:
 with dataset:
     st.header("Test header")
     st.text("Some descriptive text")
+    data = pd.read_csv(cwd + "/Car_Price_Pred.csv")
+    data.head()
 
 
 
