ec9e6d7787525d5f669eae689d7d241a16da7bc9
Shane <shanejett.ds@gmail.com>
2022-11-07 19:23:59 -0500

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -17,10 +17,10 @@ with dataset:
     st.header("Test header")
     st.text("Some descriptive text")
     data = pd.read_csv(cwd + "/Car_Price_Pred.csv")
-    data.head()
+    st.dataframe(data.head(n=10))
 
 
 
 
 
-st.write("hello, hello")
+st.write("End")
